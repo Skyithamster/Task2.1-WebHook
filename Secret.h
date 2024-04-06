102afbb7d8f8b490a7cc1ba2a30280c68a05176c
Timothy <80240004+Skyithamster@users.noreply.github.com>
2024-04-06 10:43:20 +1030

    Add files via upload

---
+++ Secret.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "Skyit"
+#define SECRET_PASS "password"
+
+#define SECRET_CH_ID 2467190			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "0V2T1DATHTSJB4FU"   // replace XYZ with your channel write API Key
